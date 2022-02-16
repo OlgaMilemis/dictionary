54588c56a1ea73af0c836bf56095eab27dc82fe2
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-02-16 20:37:43 +0100

    added axios, api call

--- src/Lexicon.js
+++ src/Lexicon.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Lexicon.css";
 
 export default function Lexicon() {
   let [inputWord, setInputword] = useState("");
 
+  function showResult(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${inputWord} definition`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`;
+
+    axios.get(apiUrl).then(showResult);
   }
+
   function handleWordInput(event) {
     setInputword(event.target.value);
   }
+
   return (
     <div className="Lexicon">
       <form onSubmit={search}>
