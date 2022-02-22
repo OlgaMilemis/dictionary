76106519476989cf13e325b50c699ed848f36b72
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-02-22 16:42:17 +0100

    Uodated Phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,7 +4,7 @@ import "./Lexicon.css";
 export default function Phonetics(props) {
   return (
     <div className="Phonetics">
-      <a href={props.phonetic.audio} rel="nonreferrer" target="_blank">
+      <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
         Listen
       </a>
       <br />
